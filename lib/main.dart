2cee76ca8752962c1a6185682d60bf0170e3493b
felipebritz <felipegustavobritz@gmail.com>
2020-11-28 08:12:00 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My First Flutter app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'My First Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _calc = 1;
+
+  _onTap() {
+    setState(() {
+      _calc = _calc * 2;
+    });
+  }
+
+  _onLongPress() {
+    setState(() {
+      _calc = 1;
+    });    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text('calc: $_calc'),
+          onTap: _onTap,
+          onLongPress: _onLongPress,
+        ),
+      )
+    );
+  }
+}
